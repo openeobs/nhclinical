cf262cf726725d425b204774fd6a2ffded83d3d3
Joel Ortiz <joelortiz@neovahealth.co.uk>
2014-06-30 12:17:14 +0100

    [IMP] utc_timestamp - converts any datetime to utc

--- t4base/orm_fixes.py
+++ t4base/orm_fixes.py
@@ -1,5 +1,12 @@
-from openerp.osv import orm
+from openerp.osv import orm, fields
+import openerp.modules.registry as registry
 import openerp
+from datetime import datetime
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT as DTF
+import logging
+import pytz
+
+_logger = logging.getLogger(__name__)
 # in this method result[groupby] is expected to be a list. In some cases int, long is returned
 # we use this fragment for workload intervals on project.task
 
@@ -85,3 +92,25 @@ def _read_group_fill_results(self, cr, uid, domain, groupby, groupby_list, aggre
 
 # METHOD REPLACEMENT
 orm.BaseModel._read_group_fill_results = _read_group_fill_results
+
+@staticmethod
+def utc_timestamp(cr, uid, timestamp, context=None):
+    assert isinstance(timestamp, datetime), 'Datetime instance expected'
+    if context and context.get('tz'):
+        tz_name = context['tz']
+    else:
+        reg = registry.RegistryManager.get(cr.dbname)
+        tz_name = reg.get('res.users').read(cr, openerp.SUPERUSER_ID, uid, ['tz'])['tz']
+    if tz_name:
+        try:
+            utc = pytz.timezone('UTC')
+            context_tz = pytz.timezone(tz_name)
+            tz_timestamp = context_tz.localize(timestamp)
+            return tz_timestamp.astimezone(utc).strftime(DTF)
+        except Exception:
+            _logger.debug("failed to compute context/client-specific timestamp, "
+                          "using the UTC value",
+                          exc_info=True)
+    return timestamp.strftime(DTF)
+
+fields.datetime.utc_timestamp = utc_timestamp
