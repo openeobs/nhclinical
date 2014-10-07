8c621a84d8a8eb55b615d9d9b2b6316843e5e327
Joel Ortiz <joelortiz@neovahealth.co.uk>
2014-10-07 15:58:53 +0100

    [IMP] Refactor. Move ews observations into a separate module nhs_ews

--- nh_clinical/__openerp__.py
+++ nh_clinical/__openerp__.py
@@ -8,7 +8,7 @@
     'description': """    """,
     'author': 'Neova Health',
     'website': 'http://www.neovahealth.co.uk/',
-    'depends': ['nh_activity', 'project', 'hr', 'nh_base'],
+    'depends': ['nh_activity', 'hr', 'nh_base'],
     'data': ['data/data.xml', 
              'views/pos_view.xml',
              'views/location_view.xml',
