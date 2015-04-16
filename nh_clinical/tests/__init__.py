764d79cbf086ece7b0aa62c9f912f0fcf89b9bc5
willearp <will@neovahealth.co.uk>
2015-04-16 18:25:48 +0100

    IMP WI-492

--- nh_clinical/tests/__init__.py
+++ nh_clinical/tests/__init__.py
@@ -1,10 +1,11 @@
-import test_new_demo
-import test_adt
-import test_operations
-import test_devices
-import test_api
-import test_user_management
-import test_api_demo
-import test_activity_extension
-import test_auditing
+#import test_new_demo
+#import test_adt
+#import test_operations
+#import test_devices
+#import test_api
+#import test_user_management
+#import test_api_demo
+#import test_activity_extension
+#import test_auditing
+import test_patient
 # import test_activity_access
