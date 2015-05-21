dc6bb7c4c6e93eb50b2d75218bd1a5aab6aa6aba
willearp <will@neovahealth.co.uk>
2015-05-21 10:54:14 +0100

    [IMP] [WI-604] added observation_extension and test

--- nh_clinical/base.py
+++ nh_clinical/base.py
@@ -602,7 +602,7 @@ class nh_clinical_patient(osv.Model):
         Checks create / update data for patients.
         Either the Hospital Number (other_identifier) or the NHS Number (patient_identifier) is required.
         On create: Hospital Number must be unique and NHS Number must be unique.
-        On update: At patient must exist with either the Hospital Number or the NHS Number provided.
+        On update: A patient must exist with either the Hospital Number or the NHS Number provided.
         changes title to res.partner.title id. Will create a new one if it does not exist.
         :return: True if successful
         """
