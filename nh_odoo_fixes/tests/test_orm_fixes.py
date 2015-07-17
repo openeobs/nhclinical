fd8c2bcd994a4b8bdb3707733f8cfeb4afa3a71c
wearp <will.earp@icloud.com>
2015-07-17 10:18:08 +0100

    [IMP][WI-831] test for utc_timestamp with tz in context

--- nh_odoo_fixes/tests/test_orm_fixes.py
+++ nh_odoo_fixes/tests/test_orm_fixes.py
@@ -1,17 +1,38 @@
 __author__ = 'wearp'
 from datetime import datetime
 import re
+import pytz
+from mock import MagicMock, patch
+
 from openerp.osv import fields
 from openerp.tests.common import TransactionCase
 
 
 class TestORMFixes(TransactionCase):
 
-    def test_utc_timestamp(self):
-        cr, uid = self.cr, self.uid
-        date = datetime.now()
-        pattern = re.compile('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}')
+    @classmethod
+    def setUpClass(cls):
+        cls.pattern = re.compile('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}')
+        cls.date = datetime.now()
 
-        timestamp = fields.datetime.utc_timestamp(cr, uid, date)
-        result = re.match(pattern, timestamp)
+    def test_01_utc_timestamp(self):
+        cr, uid = self.cr, self.uid
+
+        timestamp = fields.datetime.utc_timestamp(cr, uid, self.date)
+        result = re.match(self.pattern, timestamp)
         self.assertEquals(result.string, timestamp)
+
+    @patch('pytz.timezone')
+    def test_02_utc_timestamp_with_tz_in_context(self, timezone):
+        cr, uid = self.cr, self.uid
+        context = {'tz': 'GB'}
+        date = datetime.now()
+
+        fields.datetime.utc_timestamp(cr, uid, date, context=context)
+        self.assertEquals(timezone.call_count, 2)
+        timezone.assert_any_call('UTC')
+        timezone.assert_any_call('GB')
+        timezone.localize.called_with(date)
+
+
+
