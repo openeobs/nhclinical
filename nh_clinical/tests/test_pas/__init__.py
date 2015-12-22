793c49b87e8d036c773278c0f2de6d63d80b68fb
Will Earp <will.earp@icloud.com>
2015-12-22 14:36:23 +0000

    created test files

---
+++ nh_clinical/tests/test_pas/__init__.py
@@ -0,0 +1,13 @@
+# Part of NHClinical. See LICENSE file for full copyright and licensing details
+# coding=utf-8
+import test_patient_admit
+import test_patient_admit_cancel
+import test_patient_discharge
+import test_patient_discharge_cancel
+import test_patient_merge
+import test_patient_register
+import test_patient_swap_beds
+import test_patient_transfer
+import test_patient_transfer_cancel
+import test_patient_update
+import test_spell_update
