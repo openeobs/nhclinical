821ac36a3f84f904535fbfb11faa240adc455189
Colin Wren <colin@neovahealth.co.uk>
2016-01-03 17:49:31 +0000

    Adding models back in as guess what - these are depended on by a test - WTF?

--- nh_odoo_fixes/__init__.py
+++ nh_odoo_fixes/__init__.py
@@ -1,4 +1,5 @@
 # Part of NHClincal. See LICENSE file for full copyright and licensing details.
+from .tests import common
 from . import orm_fixes
 from . import fix_read_group_fill_results
 from . import fix_odoo8_fields_many2many_set
