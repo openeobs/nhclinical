e53fa3198acc0346fb5252ca56639b7371d3a062
Daniel Metcalfe <daniel.metcalfe@nhtek.io>
2016-09-27 15:41:27 +0100

    Added unit test as regression for EOBS-456 in which doctors being deallocated from a ward are in fact deallocated from all wards. Moved tests to do with allocations into a `user_allocation` sub-package which is intended to hold all tests of the `user_allocation.py` python module. Extracted setup logic into a method in a helper class so that it can be reused.

--- nh_clinical/tests/user_allocation/test_allocation_wizards.py
+++ nh_clinical/tests/user_allocation/test_allocation_wizards.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 from openerp.tests.common import SingleTransactionCase
 
+from . import helpers
+
 
 class TestAllocationWizards(SingleTransactionCase):
 
@@ -9,81 +11,7 @@ class TestAllocationWizards(SingleTransactionCase):
     def setUpClass(cls):
         super(TestAllocationWizards, cls).setUpClass()
         cr, uid, = cls.cr, cls.uid
-
-        cls.users_pool = cls.registry('res.users')
-        cls.groups_pool = cls.registry('res.groups')
-        cls.activity_pool = cls.registry('nh.activity')
-        cls.location_pool = cls.registry('nh.clinical.location')
-        cls.patient_pool = cls.registry('nh.clinical.patient')
-        cls.api_pool = cls.registry('nh.clinical.api')
-        cls.pos_pool = cls.registry('nh.clinical.pos')
-        cls.responsibility_allocation = cls.registry(
-            'nh.clinical.responsibility.allocation')
-        cls.staff_allocation = cls.registry('nh.clinical.staff.allocation')
-        cls.staff_reallocation = cls.registry('nh.clinical.staff.reallocation')
-        cls.doctor_allocation = cls.registry('nh.clinical.doctor.allocation')
-        cls.allocating = cls.registry('nh.clinical.allocating')
-        cls.follow_pool = cls.registry('nh.clinical.patient.follow')
-        cls.unfollow_pool = cls.registry('nh.clinical.patient.unfollow')
-
-        cls.wm_group_id = cls.groups_pool.search(
-            cr, uid, [['name', '=', 'NH Clinical Shift Coordinator Group']])
-        cls.nurse_group_id = cls.groups_pool.search(
-            cr, uid, [['name', '=', 'NH Clinical Nurse Group']])
-        cls.hca_group_id = cls.groups_pool.search(
-            cr, uid, [['name', '=', 'NH Clinical HCA Group']])
-        cls.doctor_group_id = cls.groups_pool.search(
-            cr, uid, [['name', '=', 'NH Clinical Doctor Group']])
-        cls.admin_group_id = cls.groups_pool.search(
-            cr, uid, [['name', '=', 'NH Clinical Admin Group']])
-
-        cls.hospital_id = cls.location_pool.create(
-            cr, uid, {'name': 'Test Hospital', 'code': 'TESTHOSP',
-                      'usage': 'hospital'})
-        cls.pos_id = cls.pos_pool.create(
-            cr, uid, {'name': 'Test POS', 'location_id': cls.hospital_id})
-
-        cls.admin_uid = cls.users_pool.create(
-            cr, uid, {'name': 'Admin 0', 'login': 'user_000',
-                      'password': 'user_000',
-                      'groups_id': [[4, cls.admin_group_id[0]]],
-                      'pos_id': cls.pos_id})
-
-        cls.users = {'wm': [], 'ns': [], 'hc': [], 'dr': []}
-        cls.locations = {}
-        for i in range(3):
-            wid = cls.location_pool.create(
-                cr, uid, {'name': 'Ward'+str(i), 'code': 'WARD'+str(i),
-                          'usage': 'ward', 'parent_id': cls.hospital_id,
-                          'type': 'poc'})
-            cls.locations[wid] = [cls.location_pool.create(
-                cr, uid, {'name': 'Bed'+str(i)+str(j),
-                          'code': 'BED'+str(i)+str(j), 'usage': 'bed',
-                          'parent_id': wid, 'type': 'poc'}) for j in range(10)]
-            cls.users['wm'].append(cls.users_pool.create(
-                cr, uid, {'name': 'Shift Coordinator '+str(i),
-                          'login': 'WM'+str(i),
-                          'password': 'WM'+str(i),
-                          'groups_id': [[4, cls.wm_group_id[0]]],
-                          'pos_id': cls.pos_id}))
-            cls.users['ns'].append(cls.users_pool.create(
-                cr, uid, {'name': 'Nurse '+str(i), 'login': 'NURSE'+str(i),
-                          'password': 'NURSE'+str(i),
-                          'groups_id': [[4, cls.nurse_group_id[0]]],
-                          'pos_id': cls.pos_id}))
-            cls.users['hc'].append(cls.users_pool.create(
-                cr, uid, {'name': 'HCA '+str(i), 'login': 'HCA'+str(i),
-                          'password': 'HCA'+str(i),
-                          'groups_id': [[4, cls.hca_group_id[0]]],
-                          'pos_id': cls.pos_id}))
-            cls.users['dr'].append(cls.users_pool.create(
-                cr, uid, {'name': 'Doctor '+str(i), 'login': 'DR'+str(i),
-                          'password': 'DR'+str(i),
-                          'groups_id': [[4, cls.doctor_group_id[0]]],
-                          'pos_id': cls.pos_id}))
-        cls.patients = [cls.patient_pool.create(
-            cr, uid, {'given_name': 'p'+str(k), 'family_name': 'f'+str(k),
-                      'other_identifier': 'hn'+str(k)}) for k in range(5)]
+        helpers.create_test_data(cls, cr, uid)
 
     def test_01_nursing_staff_allocation(self):
         cr, uid = self.cr, self.uid
