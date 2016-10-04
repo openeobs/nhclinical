290ce00fb474ca1e28860146a0bae9a7aa96a180
Daniel Metcalfe <daniel.metcalfe@nhtek.io>
2016-10-04 10:48:50 +0100

    For some reason the single argument version of `not_in_the_future` was being called causing a `ValidateError`. Changed the method name of the multiple argument version to be distinct in order to avoid this.
    Leaving a blank start time and populating only end time caused the `TypeError` from `not_in_the_future` to be displayed on the screen because the multiple argument version was passing the False. Changed the multiple argument version to pass over falsey values.

--- nh_odoo_fixes/validate.py
+++ nh_odoo_fixes/validate.py
@@ -5,9 +5,10 @@ from openerp.exceptions import ValidationError
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT as DTF
 
 
-def not_in_the_future(*args):
+def not_in_the_future_multiple_args(*args):
     for arg in args:
-        not_in_the_future(arg)
+        if arg:
+            not_in_the_future(arg)
 
 
 def not_in_the_future(date_time):
