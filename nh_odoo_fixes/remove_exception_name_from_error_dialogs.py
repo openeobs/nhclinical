1b578e1d21a8b3ad43a11516d89051c04e2f33b9
Daniel Metcalfe <daniel.metcalfe@nhtek.io>
2016-10-19 17:56:14 +0100

    Removed exception name from all exceptions.
    This was to make exceptions propagated to clients more user friendly and also more secure, there is no need to expose server-side implementation details to the client.

---
+++ nh_odoo_fixes/remove_exception_name_from_error_dialogs.py
@@ -0,0 +1,15 @@
+from openerp.osv.orm import except_orm
+
+
+def new_init(self, name, value):
+    self.value = value
+    # Tried `value` and `(value)` but these resulted in each character being
+    # interpreted as an arg and for some reason only the first two args are
+    # displayed in the error message client side.
+    #
+    # Also tried `(value,)` and `(value,None)` but these second args resulted
+    # in `undefined` and `null` respectively being displayed client side.
+    self.args = (value,'')
+
+except_orm.__init__ = new_init
+
