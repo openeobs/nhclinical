3551d31872f279aaee0e9758189a92e26e087b93
Daniel Metcalfe <daniel.metcalfe@nhtek.io>
2016-10-19 18:16:50 +0100

    Had to put name attribute back in because tests were depending on it.

--- nh_odoo_fixes/remove_exception_name_from_error_dialogs.py
+++ nh_odoo_fixes/remove_exception_name_from_error_dialogs.py
@@ -2,6 +2,7 @@ from openerp.osv.orm import except_orm
 
 
 def new_init(self, name, value):
+    self.name = name
     self.value = value
     # Tried `value` and `(value)` but these resulted in each character being
     # interpreted as an arg and for some reason only the first two args are
@@ -9,7 +10,6 @@ def new_init(self, name, value):
     #
     # Also tried `(value,)` and `(value,None)` but these second args resulted
     # in `undefined` and `null` respectively being displayed client side.
-    self.args = (value,'')
+    self.args = (value, '')
 
 except_orm.__init__ = new_init
-
