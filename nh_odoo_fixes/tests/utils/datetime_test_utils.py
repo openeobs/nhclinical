467c523a39745e22a8753ece08894d29939105dd
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-01-05 16:43:54 +0000

    Moved method to more appropriate model.

--- nh_odoo_fixes/tests/utils/datetime_test_utils.py
+++ nh_odoo_fixes/tests/utils/datetime_test_utils.py
@@ -10,10 +10,6 @@ class DatetimeTestUtils(TestCase):
     def runTest(self):
         pass  # To keep TestCase.__init__() happy.
 
-    @classmethod
-    def zero_datetime_seconds(cls, date_time):
-        return date_time.replace(second=0, microsecond=0)
-
     def assert_datetimes_equal_disregarding_seconds(self, expected, actual):
         """
         Useful for asserting that datetimes are within 1 minute of each other.
@@ -22,6 +18,7 @@ class DatetimeTestUtils(TestCase):
         :param actual:
         :return:
         """
-        expected = self.zero_datetime_seconds(expected)
-        actual = self.zero_datetime_seconds(actual)
+        datetime_utils = self.env['datetime_utils']
+        expected = datetime_utils.zero_datetime_seconds(expected)
+        actual = datetime_utils.zero_datetime_seconds(actual)
         self.assertEqual(expected, actual)
