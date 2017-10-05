2d1a180d6a4db5ec2bc8b40110be09e1642254e3
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-10-05 17:24:46 +0100

    Uncommented tests.

--- nh_clinical/tests/__init__.py
+++ nh_clinical/tests/__init__.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 # Part of NHClinical. See LICENSE file for full copyright and licensing details
 
-# from . import test_api_demo
-# from . import test_base_extensions
-# from . import test_location
-# from . import test_operations
-# from . import test_patient_placement_wizard
-# from . import test_responsibility_allocation_wizard
-# from . import test_users
-# from .nh_activity import *
-# from .nh_clinical_adt import *
-# from .nh_clinical_api import *
-# from .nh_clinical_doctor_allocation import *
-# from .nh_clinical_patient import *
-# from .nh_clinical_spell import *
-# from .nh_clinical_staff_allocation import *
-# from .nh_clinical_staff_reallocation import *
-# from .nh_clinical_user_management import *
-# from .res_user import *
+from . import test_api_demo
+from . import test_base_extensions
+from . import test_location
+from . import test_operations
+from . import test_patient_placement_wizard
+from . import test_responsibility_allocation_wizard
+from . import test_users
+from .nh_activity import *
+from .nh_clinical_adt import *
+from .nh_clinical_api import *
+from .nh_clinical_doctor_allocation import *
+from .nh_clinical_patient import *
 from .nh_clinical_patient_transfer import *
+from .nh_clinical_spell import *
+from .nh_clinical_staff_allocation import *
+from .nh_clinical_staff_reallocation import *
+from .nh_clinical_user_management import *
+from .res_user import *
 
 # Disabled Tests
 # from . import test_doctor
