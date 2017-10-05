4f40d2ce0683d592c846641dcf081e00ab4bfe14
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-10-05 09:43:40 +0100

    Moved test to correct layer.

--- nh_clinical/tests/nh_clinical_patient_transfer/test_patient_transfer_cancel.py
+++ nh_clinical/tests/nh_clinical_patient_transfer/test_patient_transfer_cancel.py
@@ -10,15 +10,9 @@ class TestPatientTransferCancel(TransactionCase):
         self.test_utils.admit_and_place_patient()
         self.test_utils.copy_instance_variables(self)
 
-        self.placement_model = self.env['nh.clinical.patient.placement']
         self.bed_id_before_transfer = self.patient.current_location_id
 
     def call_test(self):
-        self.placement_ward_a = self.placement_model.search([
-            ('patient_id', '=', self.patient.id),
-        ])
-        self.placement_ward_a.ensure_one()
-
         ward_b_code = self.test_utils.other_ward.code
         self.test_utils.transfer_patient(ward_b_code)
         self.bed_id_after_transfer = self.patient.current_location_id
@@ -29,22 +23,6 @@ class TestPatientTransferCancel(TransactionCase):
         ])
         transfer.ensure_one()
         transfer.cancel(transfer.activity_id.id)
-
-    def get_open_placements(self):
-        return self.placement_model.search([
-            ('patient_id', '=', self.patient.id),
-            ('state', 'not in', ['completed', 'cancelled'])
-        ])
-
-    def test_no_open_placements_when_original_bed_is_available(self):
-        """
-        If the bed the patient was in before is still
-        available, then the new, scheduled placement should
-        be cancelled leaving no open placements.
-        """
-        self.call_test()
-        placements = self.get_open_placements()
-        self.assertEqual(0, len(placements))
 
     def test_patient_back_in_original_bed_when_still_available(self):
         """
