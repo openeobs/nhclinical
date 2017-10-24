c63dfb45a80742f0e28c1aa25bfa24eef7b2b54a
Colin Wren <colin.wren@bjss.com>
2017-10-24 11:59:38 +0100

    - Linting Fixes

--- nh_odoo_fixes/docs/source/conf.py
+++ nh_odoo_fixes/docs/source/conf.py
@@ -30,12 +30,14 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.githubpages'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -171,9 +173,6 @@ texinfo_documents = [
      author, 'NHOdooFixes', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
