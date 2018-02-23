512837bed31694fe0f8b75285065b5d467c7a601
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2018-02-23 14:37:53 +0000

    Fixed a couple of code quality issues.

--- nh_clinical/static/src/js/nh_clinical.js
+++ nh_clinical/static/src/js/nh_clinical.js
@@ -3,12 +3,12 @@ openerp.nh_clinical = function(instance){
     instance.web.form.SelectCreatePopup.include({
         setup_search_view: function(search_defaults) {
             var self = this;
-            if(self.options.title.indexOf('Nursing Staff for new shift') > -1){
+            if(self.options.title.indexOf("Nursing Staff for new shift") > -1){
                 self.options.no_create = true;
             }
             self._super(search_defaults);
         },
-    })
+    });
 
     /*
     Fixes a bug that means create, edit, and delete attributes on the tree
